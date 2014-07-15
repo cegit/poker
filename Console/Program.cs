7ae55464822c9a1c4cfee22904f49d4162e345f1
cegit <christopher.erker@gmail.com>
2014-07-15 16:35:26 -0400

    Readding Console, this time NOT as a Git module, because GIT!

---
+++ Console/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PokerHand.Console
+{
+    using System;
+    using System.Linq;
+    using Models;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var deck = new Deck();
+
+            foreach (var g in deck.Cards.GroupBy(c=>c.Suit))
+            {
+                foreach (var card in g)
+                {
+                    Console.Write("{0} ", card);                    
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
